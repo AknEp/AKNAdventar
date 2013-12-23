b05f86afa8ea2ce85782cb5056a6f784edbc8f0c
AknEp <akn@s-satoshi.net>
2013-12-23 23:06:55 +0900

    Initial Commit

---
+++ AKNAdventar/AKNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKNAppDelegate.h
+//  AKNAdventar
+//
+//  Created by Satoshi SUZUKI on 2013/12/23.
+//  Copyright (c) 2013年 AknEp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AKNAdventar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AKNAdventar
+//
+//  Created by Satoshi SUZUKI on 2013/12/23.
+//  Copyright (c) 2013年 AknEp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AKNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AKNAppDelegate class]));
+    }
+}
